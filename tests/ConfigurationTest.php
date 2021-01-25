13bcb3b09122a2389987a30d5f4ac59430ffa202
Simon Podlipsky <simon@podlipsky.net>
2021-01-25 11:43:32 +0100

    Upgrade PHPStan

--- tests/ConfigurationTest.php
+++ tests/ConfigurationTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace SimPod\KafkaBundle\Tests;
 
+use Generator;
 use SimPod\KafkaBundle\Kafka\Configuration;
 
 final class ConfigurationTest extends KafkaTestCase
@@ -26,7 +27,8 @@ final class ConfigurationTest extends KafkaTestCase
         self::assertSame($clientId, $configuration->getClientId());
     }
 
-    public function providerConfiguration() : iterable
+    /** @return Generator<array{string, string|null, string, string|null}> */
+    public function providerConfiguration() : Generator
     {
         yield ['test-no-auth-no-client', null, '127.0.0.1:9092', null];
         yield ['test-sasl-auth-some-id', 'sasl-plain://user:password', '127.0.0.1:9092,127.0.1.1:9092', 'kafka-bundle'];
