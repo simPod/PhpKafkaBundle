0908dfb848951167abcb694382df41b9bce24e9a
Simon Podlipsky <simon@podlipsky.net>
2023-12-08 13:38:10 +0100

    feat: add @throws to Consumer (#94)

--- src/Kafka/Clients/Consumer/Consumer.php
+++ src/Kafka/Clients/Consumer/Consumer.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace SimPod\KafkaBundle\Kafka\Clients\Consumer;
 
+use RdKafka\Exception;
+
 interface Consumer
 {
+    /** @throws Exception */
     public function run(): void;
 }
